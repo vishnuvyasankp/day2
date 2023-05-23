09de20b65bbe60754512f078f35dc313bc165205
vishnuvyasankp <20b128@cev.ac.in>
2023-05-23 17:05:06 +0530

    first commit

---
+++ kerala.js
@@ -0,0 +1,40 @@
+let district=[
+    {
+        "id": 1,
+        "title" : "kasargod",
+        "discription" : "northmost district",
+        "image" : "./kasargod.jpeg"
+    },
+    {
+        "id": 2,
+        "title" : "kannur",
+        "discription" : "known for theyyam",
+        "image" : "./kannur.jpeg"
+    },
+    {
+        "id": 3,
+        "title" : "kozhikode",
+        "discription" : "known for beaches",
+        "image" : "./kozhikode.jpeg"
+    },
+    {
+        "id": 4,
+        "title" : "thiruvananthapuram",
+        "discription" : "capital ",
+        "image" : "./thiruvananthapuram.jpeg"
+    },
+    {
+        "id": 5,
+        "title" : "eranakulam",
+        "discription" : "queen of arabian sea",
+        "image" : "./eranakulam.jpeg"
+    },
+    {
+        "id": 6,
+        "title" : "thrissur",
+        "discription" : "famous thrissur pooram",
+        "image" : "./thrissur.jpeg"
+    },
+]
+let filtered=district.filter(e=>e,id===3)
+console.log(filt)
